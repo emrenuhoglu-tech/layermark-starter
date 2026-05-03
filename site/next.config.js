1be358564fe8975501b0c857037ffa27718dc30f
Emre Nuhoglu <emre.nuhoglu@gmail.com>
2026-05-03 20:49:25 +0200

    Add GitHub Pages deploy workflow + basePath

--- site/next.config.js
+++ site/next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
+  basePath,
+  assetPrefix: basePath,
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
